55392ef9324dc9ee6dc297d9ef7f934add6e4e4d
susmitagupta <guptasusmita24@gmail.com>
2022-11-20 18:13:48 +0530

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export class Navbar extends Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <Link className="navbar-brand" to="/">World Today</Link>
+  <Link className="navbar-brand" to="/">Genesis</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
